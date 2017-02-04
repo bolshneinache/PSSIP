9fbe48b3072ff44f83b039c349c1b50c337041ea
bolshneinache <bolshneinache@gmail.com>
2017-02-04 10:07:28 +0300

    Copy project from local storage.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+angular.module('Module',[])
+
+.controller('IndexController', ['$scope',function($scope)
+	{	
+		$scope.GetImageSrc = function()
+		{
+			$scope.picSrc="img/1.jpg";		
+		}
+		
+		$scope.ClearImageSrc = function()
+		{
+			$scope.picSrc=null;		
+		}
+		
+	}]
+);
